11fb25590a7fce5cdd652a552035296beacead9b
gryantgilgan <bryantgilgan@gmail.com>
2023-10-04 20:22:32 -0700

    adds controllers/skills.js

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,15 +1,56 @@
 const Skill = require('../models/skill');
 
-module.exports = { index, show };
+module.exports = { 
+    index: index, 
+    show: show,
+    new: newSkill,
+    create: create,
+    delete: deleteSkill,
+    edit: edit,
+    update: update 
+};
 
-function index(req, res, next) {
-    let skills = Skill.getAll();
-    res.render('skills/index', {
-        skills: skills
+function update(req, res) {
+    req.body.done = !!req.body.done;
+    Skill.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/edit', {
+        title: 'Check Yourself',
+        skill
+    });
+}
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function create(req, res) {
+    console.log(req.body);
+    Skill.create(req.body);
+    res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+    res.render('skills/new', {
+        title: 'New Skill'
     });
 }
 
 function show(req, res) {
-    let skill = Skill.getOne(req.params.id);
-    res.render('skills/show', { skill });
+    res.render('skills/show', { 
+        skill: Skill.getOne(req.params.id),
+        title: 'Inspect the Desc.'
+    });
+}
+
+function index(req, res, next) {
+    res.render('skills/index', {
+        skills: Skill.getAll(),
+        title: 'Skills that Pay the Bills'
+    });
 }
--- routes/skills.js
+++ routes/skills.js
@@ -3,8 +3,9 @@ var router = express.Router();
 const skillsCtrl = require('../controllers/skills');
 
 /* GET users listing. */
+// GET /todos
 router.get('/', skillsCtrl.index);
-
+//GET /todos/:id
 router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
